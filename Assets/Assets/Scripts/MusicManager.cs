af6fd1b709092e2b9c4d3a90ba8c75be2937a2fb
john-dev <johndevaney1@hotmail.com>
2024-12-20 03:05:32 +0000

    added title screen and idle music

--- Assets/Assets/Scripts/MusicManager.cs
+++ Assets/Assets/Scripts/MusicManager.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
 
-public class MusicMa
+
+public class MusicManager : MonoBehaviour
 {
-    
+    public AudioSource backgroundMusic;
+    public AudioSource combatMusic;
+    public float transitionSpeed = 1.0f;
+
+    private bool isInCombat = false;
+
+
+    void Update()
+    {
+        if (isInCombat)
+        {
+            // Fade out background music and fade in combat music
+            backgroundMusic.volume = Mathf.Lerp(backgroundMusic.volume, 0, Time.deltaTime * transitionSpeed);
+            combatMusic.volume = Mathf.Lerp(combatMusic.volume, 1, Time.deltaTime * transitionSpeed);
+        }
+        else
+        {
+            // Fade in background music and fade out combat music
+            backgroundMusic.volume = Mathf.Lerp(backgroundMusic.volume, 1, Time.deltaTime * transitionSpeed);
+            combatMusic.volume = Mathf.Lerp(combatMusic.volume, 0, Time.deltaTime * transitionSpeed);
+        }
+    }
+
+    public void EnterCombat()
+    {
+        isInCombat = true;
+    }
+
+    public void ExitCombat()
+    {
+        isInCombat = false;
+    }
+
 }
+
