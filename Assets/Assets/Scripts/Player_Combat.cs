72b5cafeb5f871227980414d7e9ea820108bbbd8
John Devaney <johndevaney1@hotmail.com>
2025-02-05 18:49:14 +0000

    Fixed loop issue and added timer for attacks

--- Assets/Assets/Scripts/Player_Combat.cs
+++ Assets/Assets/Scripts/Player_Combat.cs
@@ -4,18 +4,25 @@ public class Player_Combat : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Animator anim;
-    public float cooldown = 1.5f;
+    public float cooldown = 13f;
     private float timer;
 
+    public void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
     public void Attack()
     {
-        if(timer <= 0)
+        if (timer <= 0)
         {
             anim.SetBool("isAttacking", true);
             timer = cooldown;
         }
-        anim.SetBool("isAttacking", true);
     }
+
 
     public void FinishAttack()
     {
