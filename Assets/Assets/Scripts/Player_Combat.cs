e28d10c33627c7847979571a2b34c145c90b1cff
John Devaney <johndevaney1@hotmail.com>
2025-03-01 00:43:20 +0000

    Player now does damage to enemy

--- Assets/Assets/Scripts/Player_Combat.cs
+++ Assets/Assets/Scripts/Player_Combat.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Player_Combat : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Transform attackPoint;
+    public float weaponRange = 1;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
     public Animator anim;
     public float cooldown = 13f;
     private float timer;
+
 
     public void Update()
     {
@@ -13,20 +20,40 @@ public class Player_Combat : MonoBehaviour
         {
             timer -= Time.deltaTime;
         }
-    }
+    }     
     public void Attack()
     {
         if (timer <= 0)
         {
             anim.SetBool("isAttacking", true);
+
+
+            
+
             timer = cooldown;
         }
+    }
+
+    public void DealDamage(){
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+            if(enemies.Length > 0)
+            {
+                enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-damage);
+            }
     }
 
 
     public void FinishAttack()
     {
         anim.SetBool("isAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
     }
 
 }
